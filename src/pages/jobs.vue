b71d2202084f22a4e7f9340117779dcd22073926
ChitanZignuts <chintanr@zignuts.com>
2024-04-12 19:22:28 +0530

    add: job functionality

--- src/pages/jobs.vue
+++ src/pages/jobs.vue
@@ -1,9 +1,376 @@
 <script setup>
-const job="job"
+import AddJobDrawer from '@/views/apps/user/list/AddJobDrawer.vue'
+import { onMounted, ref } from 'vue'
+import { toast } from 'vue3-toastify'
+import { VDataTable } from 'vuetify/labs/VDataTable'
+import axios from '../axiosConfig'
+
+const deleteDialog = ref(false)
+const isAddJobDrawerVisible = ref(false)
+const editJobData = ref(null)
+const isEditMode = ref(false)
+const deleteItemId = ref(null)
+const jobList = ref([])
+const permentDelete=ref(false)
+const loading=ref(false)
+
+// functions for data table start
+const headers = [
+  {
+    title: 'Job Title',
+    key: 'title',
+    sortable: false,
+  },
+  {
+    title: 'Description',
+    key: 'description',
+    sortable: false,
+  },
+  {
+    title: 'Status',
+    key: 'status',
+  },
+  {
+    title: 'Posted',
+    key: 'posted',
+  },
+  {
+    title: 'Employment Type',
+    key: 'employment_type',
+  },
+  {
+    title: 'Posted By',
+    key: 'name',
+  },
+
+  {
+    title: 'ACTIONS',
+    key: 'actions',
+  },
+]
+
+const resolveJobStatus = expiryDate => {
+ 
+  const now = new Date()
+  const expirationDate = new Date(expiryDate)
+  if (!expiryDate)
+    return {
+      color: 'success',
+      text: 'Active',
+    }
+  else if (now > expirationDate)
+    return {
+      color: 'primary',
+      text: 'Expired',
+    }
+}
+
+console.log(resolveJobStatus(jobList.value.expiry_date).text)
+
+function resolveEmploymentType(employmentType) {
+  switch (employmentType) {
+  case 'Full-time':
+    return 'blue'
+  case 'Part-time':
+    return 'green'
+  case 'Contract':
+    return 'orange'
+  case 'Freelance':
+    return 'purple'
+  case 'Internship':
+    return 'yellow'
+  case 'Remote':
+    return 'teal'
+  default:
+    return 'black' 
+  }
+}
+
+function getTimeAgo(date) {
+  const now = new Date()
+  const postedDate = new Date(date)
+  const timeDifference = now.getTime() - postedDate.getTime()
+  const seconds = Math.floor(timeDifference / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+  const years = Math.floor(days / 365)
+
+  if (years > 0) {
+    return years === 1 ? '1 year ago' : `${years} years ago`
+  } else if (days > 0) {
+    return days === 1 ? '1 day ago' : `${days} days ago`
+  } else if (hours > 0) {
+    return hours === 1 ? '1 hour ago' : `${hours} hours ago`
+  } else if (minutes > 0) {
+    return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`
+  } else {
+    return 'Just now'
+  }
+}
+
+// functions for data table end
+
+//function for fetch data of job
+const fetchData = async () => {
+  loading.value=true
+  try {
+
+    const token = localStorage.getItem('token')
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    const response = await axios.get('jobs/company', config)
+
+    jobList.value = response.data.data
+  } catch (error) {
+    console.error('Failed to fetch job data:', error.message)
+    toast.error("Failed to fetch Job data")
+  }
+  loading.value=false
+}
+
+//function for fetch data for edit and open drawer
+const openAddJobDrawer =async jobData => {
+  if (jobData) {
+    try {
+      const token = localStorage.getItem('token')
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`, 
+          'content-type': 'multipart/form-data',
+        },
+      }
+
+      const response = await axios.get(`/job/${jobData.id}`, config)
+
+      
+      editJobData.value = response.data.data
+      if(editJobData.value){
+        isEditMode.value = true
+        isAddJobDrawerVisible.value = true
+      }
+      
+    } catch (error) {
+      console.error('Failed to fetch company details:', error.message)
+      toast.error("Failed to fetch company data")
+    }
+  } else {
+    editJobData.value = null
+    isEditMode.value = false
+    isAddJobDrawerVisible.value = true
+  }
+}
+
+const deleteItem = item => {
+  deleteItemId.value=item
+  deleteDialog.value = true
+}
+
+const closeDelete = () => {
+  deleteDialog.value = false
+}
+
+const deleteItemConfirm = async () => {
+  try {
+
+    const token = localStorage.getItem('token')
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    await axios.post(`job/delete/${deleteItemId.value}`, { permanent: permentDelete.value }, config)
+
+    fetchData()
+    closeDelete()
+    toast.success("Company Deleted Successfully")
+  } catch (error) {
+    console.error('Failed to delete company:', error.message)
+    toast.error(error.message)
+  } 
+}
+
+//function that used by child component on form submit
+const addNewJob = async jobData => {
+  loading.value=true
+  try {
+    const token = localStorage.getItem('token')
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    if (isEditMode.value) {
+      
+      const response = await axios.post(`job/update/${editJobData.value.id}`, jobData, config)
+
+     
+      console.log('User updated successfully:', response.data)
+      toast.success(response.data.message)
+    } else {
+      
+      const response = await axios.post('job/create', jobData, config)
+
+      toast.success(response.data.message)
+    }
+
+    fetchData()
+   
+    isAddJobDrawerVisible.value = false
+  } catch (error) {
+    console.error('Failed to update or create user:', error.message)
+    toast.error(error.message)
+  }
+  loading.value=false
+}
+
+
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <template>
   <div>
-    hii
+    <!-- <Snackbar /> -->
+    <div
+      v-if="loading"
+      class="d-flex justify-center"
+    >
+      <VProgressCircular
+        :size="40"
+        color="primary"
+        indeterminate
+      />
+    </div>
+    <!-- 👉 Add user button -->
+    <div v-else>
+      <div class="d-flex justify-end ma-3">
+        <VBtn
+          prepend-icon="tabler-plus"
+          @click="openAddJobDrawer(null)"
+        >
+          Add New Job
+        </VBtn>
+      </div>
+      <VDataTable
+        :headers="headers"
+        :items="jobList"
+        :items-per-page="10"
+      >
+        <!-- title column -->
+        <template #item.title="{ item }">
+          <div class="d-flex align-center">
+            <div class="d-flex flex-column ms-3">
+              <span class="d-block font-weight-medium text--primary text-truncate">{{ item.raw.title }}</span>
+            </div>
+          </div>
+        </template>
+
+        <!-- description column -->
+        <template #item.description="{ item }">
+          <span>{{ item.raw.description }}</span>
+        </template>
+
+        <template #item.status="{ item }">
+          <VChip
+            :color="resolveJobStatus(item.expiry_date).color"
+            size="small"
+            label
+            class="text-capitalize"
+          >
+            {{ resolveJobStatus(item.expiry_date).text }}
+          </VChip>
+        </template>
+        <template #item.posted="{ item }">
+          <span>{{ getTimeAgo( item.raw.posted_date) }}</span>
+        </template>
+
+        <!-- company name column -->
+        <template #item.employment_type="{ item }">
+          <VChip
+            color="primary"
+            size="small"
+            label
+            class="text-capitalize"
+          >
+            {{ item.raw.employment_type }}
+          </VChip>
+        </template>
+        <template #item.name="{ item }">
+          <VChip pill>
+            <VAvatar
+              start
+              :image="item.raw.company.logo_url?`http://127.0.0.1:8000/storage/logos/${item.raw.company.logo_url}`:null"
+            />
+            <span>{{ item.raw.company.name }}</span>
+          </VChip>
+        </template>
+
+        <!-- Status column -->
+        
+
+        <!-- Actions column -->
+        <template #item.actions="{ item }">
+          <div class="d-flex gap-1">
+            <IconBtn @click="openAddJobDrawer(item.raw)">
+              <VIcon icon="mdi-pencil-outline" />
+            </IconBtn>
+            <IconBtn @click="deleteItem(item.raw.id)">
+              <VIcon icon="mdi-delete-outline" />
+            </IconBtn>
+          </div>
+        </template>
+      </VDataTable>
+    </div>
+    <VDialog
+      v-model="deleteDialog"
+      max-width="500px"
+    >
+      <VCard class="align-center d-flex justify-center ma-5">
+        <VCardTitle>
+          Are you sure you want to delete this item?
+        </VCardTitle>
+        <div class="demo-space-x ">
+          <VCheckbox
+            v-model="permentDelete"
+            label=" Delete Company Permanent"
+          />
+        </div>
+        <VCardActions class="mt-5">
+          <VSpacer />
+          <VBtn
+            color="error"
+            variant="outlined"
+            @click="closeDelete"
+          >
+            Cancel
+          </VBtn>
+          <VBtn
+            color="success"
+            variant="elevated"
+            @click="deleteItemConfirm"
+          >
+            OK
+          </VBtn>
+          <VSpacer />
+        </VCardActions>
+      </VCard>
+    </VDialog>
+    <AddJobDrawer
+      v-model:isJobDrawerOpen="isAddJobDrawerVisible"
+      :job-data="editJobData"
+      @job-data="addNewJob"
+    />
   </div>
 </template>
---
+++ src/views/apps/user/list/AddJobDrawer.vue
@@ -0,0 +1,230 @@
+<script setup>
+import { requiredValidator } from "@validators"
+import { onMounted, ref } from "vue"
+import { PerfectScrollbar } from "vue3-perfect-scrollbar"
+import axios from "../../../../axiosConfig"
+
+const props = defineProps({
+  isJobDrawerOpen: {
+    type: Boolean,
+    required: true,
+  },
+  jobData: {
+    type: Object,
+    default: null,
+  },
+})
+
+const emit = defineEmits(["update:isJobDrawerOpen", "jobData"])
+const companyOptions = ref([])
+const isFormValid = ref(false)
+const refForm = ref()
+
+//ref for form data
+const Title= ref("")
+const Description = ref("")
+const Salary = ref("")
+const EmploymentType = ref("")
+const RequiredExperience = ref("")
+const ExpiryDate = ref(null)
+const CompanyId = ref(null)
+
+
+const clearForm = () => {
+  refForm.value?.reset()
+  ExpiryDate.value=""
+  refForm.value?.resetValidation()
+}
+
+const closeNavigationDrawer = () => {
+  emit("update:isJobDrawerOpen", false)
+  clearForm()
+}
+
+watch(
+  () => props.jobData,
+  newValue => {
+    if (newValue) {
+      Title.value = newValue.title
+      Description.value = newValue.description
+      Salary.value = newValue.salary
+      EmploymentType.value = newValue.employment_type
+      RequiredExperience.value = newValue.required_experience
+      ExpiryDate.value = newValue.expiry_date
+      CompanyId.value = newValue.company.id
+    } else {
+      clearForm()
+    }
+  },
+)
+
+const onSubmit = async () => {
+  try {
+    let validate = await refForm.value?.validate()
+    console.log(validate)
+    if (validate.valid) {
+      const formData = {
+        title: Title.value,
+        description: Description.value,
+        salary: Salary.value,
+        employment_type: EmploymentType.value,
+        required_experience: RequiredExperience.value,
+        expiry_date: ExpiryDate.value,
+      }
+
+      if (!props.jobData) {
+        formData.company_id = CompanyId.value
+      }
+      emit("jobData", formData)
+      closeNavigationDrawer()
+      nextTick(() => {
+        clearForm()
+        
+      })
+    }
+  } catch (error) {
+    notify("error", "Failed to submit form")
+    console.error("Error:", error.message)
+  }
+}
+
+const fetchCompanyOptions = async () => {
+
+  try {
+    const token = localStorage.getItem("token")
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    const response = await axios.get("employee/companies/option", config)
+   
+    companyOptions.value = response.data
+    
+   
+    
+  } catch (error) {
+    console.error("Error fetching company options:", error)
+  }
+}
+
+const handleDrawerModelValueUpdate = val => {
+  emit("update:isJobDrawerOpen", val)
+}
+
+onMounted(() => {
+  fetchCompanyOptions()
+})
+</script>
+
+<template>
+  <VNavigationDrawer
+    temporary
+    :width="400"
+    location="end"
+    class="scrollable-content"
+    :model-value="props.isJobDrawerOpen"
+    @update:model-value="handleDrawerModelValueUpdate"
+  >
+    <!-- 👉 Title -->
+    <AppDrawerHeaderSection
+      :title="props.jobData ? 'Edit Job' : 'Add Job'"
+      @cancel="closeNavigationDrawer"
+    />
+
+    <PerfectScrollbar :options="{ wheelPropagation: false }">
+      <VCard flat>
+        <VCardText>
+          <VForm
+            ref="refForm"
+            v-model="isFormValid"
+            @submit.prevent="onSubmit"
+          >
+            <VRow>
+              <!-- 👉 Full name -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="Title"
+                  :rules="[requiredValidator]"
+                  label="Title"
+                />
+              </VCol>
+              <VCol cols="12">
+                <AppTextarea
+                  v-model="Description"
+                  clearable
+                  clear-icon="tabler-circle-x"
+                  :rules="[requiredValidator]"
+                  label="Description"
+                />
+              </VCol>
+              <VCol cols="12">
+                <AppAutocomplete
+                  v-model="EmploymentType"
+                  label="Employment Type"
+                  :items="EmploymentOptions"
+                  item-title="name"
+                  item-value="id"
+                  :rules="[requiredValidator]"
+                  :disabled="props.jobData !== null"
+                />
+              </VCol>
+              <VCol cols="12">
+                <AppTextField
+                  v-model="Salary"
+                  label="Salary"
+                  type="number"
+                />
+              </VCol>
+              <!-- 👉 Required Experience -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="RequiredExperience"
+                  :rules="[requiredValidator]"
+                  label="Required Experience"
+                />
+              </VCol>
+              <VCol cols="12">
+                <AppDateTimePicker
+                  v-model="ExpiryDate"
+                  placeholder="YYYY-MM-DD"
+                  :config="{ dateFormat: 'Y-m-d', minDate: new Date() }"
+                  label="Expiry Date"
+                />
+              </VCol>
+              <VCol cols="12">
+                <AppAutocomplete
+                  v-model="CompanyId"
+                  label="Company"
+                  :items="companyOptions"
+                  item-title="name"
+                  item-value="id"
+                  :rules="[requiredValidator]"
+                  :disabled="props.jobData !== null"
+                />
+              </VCol>
+              <!-- 👉 Submit and Cancel -->
+              <VCol cols="12">
+                <VBtn
+                  type="submit"
+                  class="me-3"
+                >
+                  Submit
+                </VBtn>
+                <VBtn
+                  variant="tonal"
+                  color="secondary"
+                  @click="closeNavigationDrawer"
+                >
+                  Cancel
+                </VBtn>
+              </VCol>
+            </VRow>
+          </VForm>
+        </VCardText>
+      </VCard>
+    </PerfectScrollbar>
+  </VNavigationDrawer>
+</template>
